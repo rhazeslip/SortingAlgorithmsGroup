edd2d5606554ce7c9d0f06a6414256a1abe46d55
rhazeslip <rhazeslip@cub.uca.edu>
2025-09-24 11:12:04 -0500

    Entered Commits for dividing work

--- SortAlgorithms.py
+++ SortAlgorithms.py
@@ -1,1 +1,19 @@
+#This program implements various sorting algorithms and compares their efficiency based on 
+#excution time using various data size sets.
+
+
+#Bubble Sort Algorithm (Nate Hazeslip)
+
+
+#Quick Sort Algorithm (Nate Hazeslip)
+
+
+#Merge Sort Algorithm (Kaden Hyde)
+
+
+#Alternate Sorting Algorithm (Seth Wojcik)
+
+
+
+#Main Function to test and run algorithms to compare efficiency
 
